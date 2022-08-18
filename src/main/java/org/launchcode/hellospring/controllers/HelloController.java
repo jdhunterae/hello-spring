3086b5fb5112916a632e4244ace89a4ef18f3f71
Andrew Pomerleaum <arpomerleau@gmail.com>
2022-08-18 15:30:46 -0400

    Initial commit

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,36 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+    @GetMapping("goodbye")
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("form")
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='/hello' method='post'>" +
+                "<input type='text' name='name'/>" +
+                "<input type='submit' value='Greet me!'/>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+}
---
+++ src/test/java/org/launchcode/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,11 @@
+package org.launchcode.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloSpringApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
