73b8ec65ee3d0e00ab62f2095fda00cc6e32ad1c
Andrew Pomerleau <arpomerleau@gmail.com>
2022-08-18 19:15:13 -0400

    Utilizing static Strings to build language select field in hello/form and to select the appropriate greeting based on language selected.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,23 +3,37 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 @Controller
 @ResponseBody
 @RequestMapping("hello")
 public class HelloController {
+    private static final String[] LANGUAGES = {"English", "French", "Spanish", "German", "Italian"};
+    private static final HashMap<String, String> GREETINGS;
+
+    static {
+        GREETINGS = new HashMap<String, String>();
+        GREETINGS.put("English", "Hello");
+        GREETINGS.put("French", "Bonjour");
+        GREETINGS.put("German", "Guten tag");
+        GREETINGS.put("Spanish", "Hola");
+        GREETINGS.put("Italian", "Ciao");
+    }
+
     @GetMapping("goodbye")
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
+        return greet(name, language);
     }
 
     @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+        return greet(name, LANGUAGES[0]);
     }
 
     @GetMapping("form")
@@ -28,9 +42,26 @@ public class HelloController {
                 "<body>" +
                 "<form action='/hello' method='post'>" +
                 "<input type='text' name='name'/>" +
+                getSelector() +
                 "<input type='submit' value='Greet me!'/>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    private static String getSelector() {
+        StringBuilder res = new StringBuilder();
+
+        res.append("<select name='language'>");
+        for (String language : LANGUAGES) {
+            res.append(String.format("<option value='%s'>%s</option>", language, language));
+        }
+        res.append("</select>");
+
+        return res.toString();
+    }
+
+    private static String greet(String name, String language) {
+        return String.format("%s, %s", GREETINGS.get(language), name);
     }
 }
